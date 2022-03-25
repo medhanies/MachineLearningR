e856ff04d18e1610787bfffd20b49e28ab9b9702
medhanies <82340876+medhanies@users.noreply.github.com>
2022-03-25 02:07:54 -0500

    Add files via upload

---
+++ decision_tree_regression.R
@@ -0,0 +1,31 @@
+# Decision Tree Regression
+library(rpart)
+library(ggplot2)
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting Decision Tree Regression to the dataset
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting a new result with Decision Tree Regression
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Decision Tree Regression results (higher resolution)
+# install.packages('ggplot2')
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Plotting the tree
+plot(regressor)
+text(regressor)
